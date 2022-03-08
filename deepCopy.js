d38a508dc40053134e5c7130486a0fb7eeebea7a
nicojpargade <88997481+nicojpargade@users.noreply.github.com>
2022-03-08 14:24:45 -0300

    Create deepCopy.js

---
+++ deepCopy.js
@@ -0,0 +1,36 @@
+function deepCopy(subject) {
+    function isArray(subject) {
+        return Array.isArray(subject);
+    }
+
+    function isObject(subject) {
+        return typeof subject == "object";
+    }
+
+    let copySubject;
+
+    const subjectIsObject = isObject(subject);
+    const subjectIsArray = isArray(subject);
+
+    if (subjectIsArray) {
+        copySubject = [];
+    } else if (subjectIsObject) {
+        copySubject = {};
+    } else {
+        return subject;
+    }
+
+    for (key in subject) {
+        const keyIsObject = isObject(subject[key]);
+        if (keyIsObject) {
+            copySubject[key] = deepCopy(subject[key]);
+        } else {
+            if (subjectIsArray) {
+                copySubject.push(subject[key]);
+            } else {
+                copySubject[key] = subject[key];
+            }
+        }
+    }
+    return copySubject;
+}
